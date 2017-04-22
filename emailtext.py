da07b1c3fe5d8deaab512862741ba0753cc10c28
mindietea <mindychi@gmail.com>
2017-04-22 14:23:26 -0500

    Add files via upload
    Simple python emailing

---
+++ emailtext.py
@@ -0,0 +1,33 @@
+# Sending function
+import smtplib
+
+# Email module
+from email.mime.text import MIMEText
+
+# These work, enjoy your new gmail account
+donut = "willycheestick6@gmail.com"
+pwd = "raymondchee"
+
+# Recipient
+to = "copperstick6@gmail.com"
+
+def sendEmail(txt):
+
+	print("Sending email...\n")
+	smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo
+	smtpserver.login(donut, pwd)
+
+	header = "To: " + to + "\n" + "From: " + donut
+	header = header + "\n" + "Subject: Donut Reply :-)\n"
+
+	msg = header + "\n" + txt + "\n\n"
+	smtpserver.sendmail(donut, to, msg)
+
+	print()
+
+	smtpserver.close()
+
+	return
